4101aa011af25ebd59968c73e71bb986d8edd1ec
David Dassau <todieahero@hotmail.com>
2018-02-24 11:36:47 -0600

    everything is fixed now

--- ADOExample/Program.cs
+++ ADOExample/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Data;
-using System.Data.SqlClient;
+using ADOExample.DataAccess;
 
 namespace ADOExample
 {
@@ -8,34 +7,17 @@ namespace ADOExample
     {
         static void Main(string[] args)
         {
-
             var firstLetter = Console.ReadLine();
 
-            using (var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;")) // set up connection
+            var invoiceQuery = new InvoiceQuery();
+            var invoices = invoiceQuery.GetInvoiceByTrackFirstLetter(firstLetter);
+
+            foreach (var invoice in invoices)
             {
-                connection.Open(); // opens connection
-                var cmd = connection.CreateCommand(); // vehicle to set up our query
-                cmd.CommandText = @"select x.invoiceid,BillingAddress
-                                from invoice i
-                                    join InvoiceLine x 
-                                        on x.InvoiceId = i.InvoiceId
-                                where exists (select TrackId from Track 
-                                              where Name like @FirstLetter + '%' and TrackId = x.TrackId)";
-
-                var firstLetterParam = new SqlParameter("@FirstLetter", SqlDbType.NVarChar);
-                firstLetterParam.Value = firstLetter;
-                cmd.Parameters.Add(firstLetterParam);
-
-                var reader = cmd.ExecuteReader(); // sql data reader, that reads the results from our sql query
-
-                while (reader.Read()) // while there are rows, do this thing
-                {
-                    var invoiceId = reader.GetInt32(0);
-                    var billingAddress = reader["BillingAddress"].ToString();
-
-                    Console.WriteLine($"Invoice {invoiceId} is going to {billingAddress}");
-                }
+                Console.WriteLine($"Invoice Id {invoice.InvoiceId} was shipped to {invoice.BillingAddress}.");
             }
+
+
 
             Console.ReadLine();
 
